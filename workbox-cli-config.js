3108e0313bb105b35b19016f8e54651e0b16f645
Julius Naperkowski <julius@nepos.io>
2017-12-07 16:37:28 +0100

    added first service worker implementation based on googles workbox-sw

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{js,css,html}"
+  ],
+  "swDest": "build/sw.js",
+  "globIgnores": [
+    "../workbox-cli-config.js"
+  ]
+};
