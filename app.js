934af151ed902fc22353e32b76bc5090357c16c7
Julius Naperkowski <julius@nepos.io>
2018-01-25 13:07:17 +0100

    don’t serve /build (obsolete)

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/dist'));
 
 module.exports = app;
